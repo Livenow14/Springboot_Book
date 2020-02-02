eadd99ed7dc5e8d79ef39441a20b3026827e13d5
Livenow14 <xowjd41@naver.com>
2020-02-03 01:30:20 +0900

    Posts registration Api Test

--- src/test/java/com/jojoIdu/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoIdu/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -1,6 +1,4 @@
 package com.jojoIdu.book.springboot.web.domain.posts;
-
-
 import com.jojoIdu.book.springboot.domain.posts.Posts;
 import com.jojoIdu.book.springboot.domain.posts.PostsRepository;
 import org.junit.After;
@@ -9,10 +7,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,12 +21,11 @@ public class PostsRepositoryTest {
     @After
     public void cleanup(){
         postsRepository.deleteAll();
-    }
+}
 
     @Test
     public void 게시글저장_불러오기() {
         //given
-
         String title="테스트 게시글";
         String content = "테스트 본문";
 
@@ -39,15 +35,8 @@ public class PostsRepositoryTest {
         List<Posts> postsList = postsRepository.findAll();
 
         //then
-
         Posts posts = postsList.get(0);
-        assertThat(posts.getTitle().isEqualTo(title));
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
     }
-
-
-
-
-
-
-
 }
