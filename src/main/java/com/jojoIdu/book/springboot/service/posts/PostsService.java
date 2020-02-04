57a70413bb586f6230c84ddb3903d51f879d1548
Livenow14 <xowjd41@naver.com>
2020-02-04 13:36:13 +0900

    등록/수정/조희 기능 완료, h2-console -> jdbc:h2:mem:testdb

--- src/main/java/com/jojoIdu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoIdu/book/springboot/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package com.jojoIdu.book.springboot.service.posts;
 
 
+import com.jojoIdu.book.springboot.domain.posts.Posts;
 import com.jojoIdu.book.springboot.domain.posts.PostsRepository;
+import com.jojoIdu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoIdu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoIdu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+    @Transactional
+    public PostsResponseDto findById(Long id){
+        Posts entity=postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 사용자가 없습니다.. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
